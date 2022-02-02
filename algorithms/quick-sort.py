9a53bd1bc3092d2c76292dbdc0b15eb71d8a1dfd
rfbatista <renfbatista@gmail.com>
2022-02-01 21:30:43 -0300

    quick sort in python

---
+++ algorithms/quick-sort.py
@@ -0,0 +1,10 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
+  
+  
+print(quicksort([1,66,3,55,7,234,13,343]))
